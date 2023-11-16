d15096f0d89e2d60b12439fc8245b7c5dc5b0cdd
RubsRafa <rubiarafaela2000@gmail.com>
2023-11-16 19:37:43 -0300

    feat: inicio and sistemas page

--- src/HomePage.py
+++ src/HomePage.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-import InícioPage as Inicio
+from tkinter import ttk
 
 class HomePage(tk.Tk):
     def __init__(self):
@@ -10,7 +10,7 @@ class HomePage(tk.Tk):
         self.bar = tk.Frame(self, bg='#535353', width=200)
         self.bar.pack(side=tk.LEFT, fill=tk.Y)
 
-        options = ['Início','Sistemas', 'Perfis de Acesso', 'Matriz SoD']
+        options = ['Início', 'Sistemas', 'Perfis de Acesso', 'Matriz SoD']
         for item in options:
             self.btn = tk.Button(self.bar, text=item, command=lambda text=item: self.show_button(text), foreground='gray', bg='black', activebackground='gray', activeforeground='white', font=('Roboto', 14), borderwidth=0)
             self.btn.pack(pady=10, padx=10)
@@ -18,24 +18,94 @@ class HomePage(tk.Tk):
         self.main_content = tk.Frame(self, bg='red')
         self.main_content.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True)
 
+        self.pages = {
+            'Início': InicioPage,
+            'Sistemas': SistemasPage,
+            'Perfis de Acesso': lambda: self.update_main_content('Conteúdo da Página de Perfis de Acesso', 'purple'),
+            'Matriz SoD': lambda: self.update_main_content('Conteúdo da Página de Matriz SoD', 'yellow')
+        }
+
+        self.show_page('Início')
+
     def show_button(self, button_text):
-        if button_text == 'Início':
-            self.update_main_content('Conteúdo da Página de Início', 'red')
-        elif button_text == 'Sistemas':
-            self.update_main_content('Conteúdo da Página de Sistemas', 'green')
-        elif button_text == 'Perfis de Acesso':
-            self.update_main_content('Conteúdo da Página de Perfis de Acesso', 'purple')
-        elif button_text == 'Matriz SoD':
-            self.update_main_content('Conteúdo da Página de Matriz SoD', 'yellow')
-        
+        self.show_page(button_text)
+
     def update_main_content(self, content, color):
         if self.main_content:
             self.main_content.destroy()
-        
+
         self.main_content = tk.Frame(self, bg=color)
         label = tk.Label(self.main_content, text=content, font=('Roboto', 18), foreground='white', bg='gray')
         label.pack(padx=20, pady=20)
         self.main_content.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True)
 
+    def show_page(self, page_name):
+        if page_name in self.pages:
+            page = self.pages[page_name]()
+            self.main_content.destroy()
+            self.main_content = page
+            self.main_content.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True)
 
+
+class InicioPage(tk.Frame):
+    def __init__(self):
+        super().__init__()
+
+        self.content = (
+            "Início\n"
+            "Adicionar mais texto"
+        )
+        self.color = 'red'
+
+        self.configure(bg=self.color)
+        label = tk.Label(self, text=self.content, font=('Roboto', 14), foreground='white', bg='gray')
+        label.pack(padx=20, pady=20)
+
+
+class SistemasPage(tk.Frame):
+    def __init__(self):
+        super().__init__()
+
+        self.create_widgets()
+
+    def create_widgets(self):
+        columns = ('Código do Sistema', 'Nome do Sistema')
+        self.tree = ttk.Treeview(self, columns=columns, show='headings', height=10)
+
+        for col in columns:
+            self.tree.heading(col, text=col)
+            self.tree.column(col, width=150)
+
+        self.tree.pack(pady=20)
+
+        self.codigo_entry = tk.Entry(self)
+        self.nome_entry = tk.Entry(self)
+
+        add_button = tk.Button(self, text='Adicionar', command=self.adicionar_sistema)
+        remove_button = tk.Button(self, text='Remover', command=self.remover_sistema)
+
+        self.codigo_entry.pack(pady=10)
+        self.nome_entry.pack(pady=10)
+        add_button.pack(pady=10)
+        remove_button.pack(pady=10)
+
+    def adicionar_sistema(self):
+        codigo = self.codigo_entry.get()
+        nome = self.nome_entry.get()
+
+        if codigo and nome:
+            self.tree.insert('', 'end', values=(codigo, nome))
+            self.codigo_entry.delete(0, 'end')
+            self.nome_entry.delete(0, 'end')
+
+    def remover_sistema(self):
+        selected_item = self.tree.selection()
+
+        if selected_item:
+            self.tree.delete(selected_item)
+
+
+if __name__ == "__main__":
+    app = HomePage()
+    app.mainloop()
 
