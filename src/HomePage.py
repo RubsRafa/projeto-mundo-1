a8322ef71a8b1f9ed5b2f9d71293bca18957544d
RubsRafa <rubiarafaela2000@gmail.com>
2023-11-16 20:05:57 -0300

    feat: access profiles page

--- src/HomePage.py
+++ src/HomePage.py
@@ -21,7 +21,7 @@ class HomePage(tk.Tk):
         self.pages = {
             'Início': InicioPage,
             'Sistemas': SistemasPage,
-            'Perfis de Acesso': lambda: self.update_main_content('Conteúdo da Página de Perfis de Acesso', 'purple'),
+            'Perfis de Acesso': PerfisPage,
             'Matriz SoD': lambda: self.update_main_content('Conteúdo da Página de Matriz SoD', 'yellow')
         }
 
@@ -80,25 +80,91 @@ class SistemasPage(tk.Frame):
 
         self.tree.pack(pady=20)
 
-        self.codigo_entry = tk.Entry(self)
-        self.nome_entry = tk.Entry(self)
+        self.code_entry = tk.Entry(self)
+        self.name_entry = tk.Entry(self)
 
         add_button = tk.Button(self, text='Adicionar', command=self.adicionar_sistema)
         remove_button = tk.Button(self, text='Remover', command=self.remover_sistema)
 
-        self.codigo_entry.pack(pady=10)
-        self.nome_entry.pack(pady=10)
+        self.code_entry.pack(pady=10)
+        self.name_entry.pack(pady=10)
         add_button.pack(pady=10)
         remove_button.pack(pady=10)
 
     def adicionar_sistema(self):
-        codigo = self.codigo_entry.get()
-        nome = self.nome_entry.get()
+        codigo = self.code_entry.get()
+        nome = self.name_entry.get()
 
         if codigo and nome:
             self.tree.insert('', 'end', values=(codigo, nome))
-            self.codigo_entry.delete(0, 'end')
-            self.nome_entry.delete(0, 'end')
+            self.code_entry.delete(0, 'end')
+            self.name_entry.delete(0, 'end')
+
+    def remover_sistema(self):
+        selected_item = self.tree.selection()
+
+        if selected_item:
+            self.tree.delete(selected_item)
+
+
+class PerfisPage(tk.Frame):
+    def __init__(self):
+        super().__init__()
+
+        self.create_widgets()
+
+    def create_widgets(self):
+        columns = ('Código do Sistema', 'Nome do Sistema', 'Descrição')
+        self.tree = ttk.Treeview(self, columns=columns, show='headings', height=10)
+
+        for col in columns:
+            self.tree.heading(col, text=col)
+            self.tree.column(col, width=150)
+
+        self.tree.pack(pady=20)
+
+        # self.code_label = tk.Label(self, text='Código do Sistema', bg='black', foreground='white', font=("Calibri", 12))
+        # self.code_label.grid(row=1, column=1, padx=5, pady=5, sticky='e')
+        self.code_entry = tk.Entry(self)
+        # self.code_entry.grid(row=1, column=2, padx=5, pady=5, sticky='w')
+
+        # self.name_label = tk.Label(self, text='Nome do Sistema', bg='black', foreground='white', font=("Calibri", 12))
+        # self.name_label.grid(row=1, column=1, padx=5, pady=5, sticky='e')
+        self.name_entry = tk.Entry(self)
+        # self.name_entry.grid(row=1, column=2, padx=5, pady=5, sticky='w')
+
+        # self.description_label = tk.Label(self, text='Descrição', bg='black', foreground='white', font=("Calibri", 12))
+        # self.description_label.grid(row=1, column=1, padx=5, pady=5, sticky='e')
+        self.description_entry = tk.Entry(self)
+        # self.description_entry.grid(row=1, column=2, padx=5, pady=5, sticky='w')
+
+        code_placeholder = 'Código do Sistema'
+        name_placeholder = 'Nome do Sistema'
+        description_placeholder = 'Descrição'
+        self.code_entry.insert(0, code_placeholder)
+        self.name_entry.insert(0, name_placeholder)
+        self.description_entry.insert(0, description_placeholder)
+
+
+        add_button = tk.Button(self, text='Adicionar', command=self.adicionar_sistema)
+        remove_button = tk.Button(self, text='Remover', command=self.remover_sistema)
+
+        self.code_entry.pack(pady=10)
+        self.name_entry.pack(pady=10)
+        self.description_entry.pack(pady=10)
+        add_button.pack(pady=10)
+        remove_button.pack(pady=10)
+
+    def adicionar_sistema(self):
+        code = self.code_entry.get()
+        name = self.name_entry.get()
+        description = self.description_entry.get()
+
+        if code and name and description:
+            self.tree.insert('', 'end', values=(code, name, description))
+            self.code_entry.delete(0, 'end')
+            self.name_entry.delete(0, 'end')
+            self.description_entry.delete(0, 'end')
 
     def remover_sistema(self):
         selected_item = self.tree.selection()
